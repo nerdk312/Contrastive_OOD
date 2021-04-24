16d272ba940ab9e2e70df273f0c12c22570176c5
nerdk312 <nawid.keshtmand@hotmail.com>
2021-04-24 19:16:29 +0100

    basic module for NPID basically complete

--- Contrastive_uncertainty/npid/models/npid_module.py
+++ Contrastive_uncertainty/npid/models/npid_module.py
@@ -7,10 +7,12 @@ import torchvision
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 
+
 from Contrastive_uncertainty.npid.models.resnet_models import custom_resnet18,custom_resnet34,custom_resnet50
-from Contrastive_uncertainty.npid.models.loss_functions import moco_loss, supervised_contrastive_loss
+from Contrastive_uncertainty.npid.models.simple_memory import SimpleMemory
+from Contrastive_uncertainty.npid.utils.pl_metrics import precision_at_k
 
-
+# Based on code from https://github.com/open-mmlab/OpenSelfSup/blob/master/openselfsup/models/npid.py
 class NPIDModule(pl.LightningModule):
     def __init__(self,
         emb_dim: int = 128,
@@ -32,13 +34,15 @@ class NPIDModule(pl.LightningModule):
         self.save_hyperparameters()
         self.datamodule = datamodule
 
+        # Instantiate memory bank
+        self.memory_bank = SimpleMemory(feat_dim=emb_dim, momentum=encoder_momentum)
+
         # create the encoders
         # num_classes is the output fc dimension
         self.encoder= self.init_encoders()
         if use_mlp:  # hack: brute-force replacement
             dim_mlp = self.encoder.fc.weight.shape[1]
             self.encoder.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_q.fc)
-            
             
         if self.hparams.pretrained_network is not None:
             self.encoder_loading(self.hparams.pretrained_network)
@@ -64,9 +68,66 @@ class NPIDModule(pl.LightningModule):
         Output:
             z: latent vector
         """
-        z = self.encoder_k(x)
+        z = self.encoder(x)
         z = nn.functional.normalize(z, dim=1)
         return z
+
+    
+    def forward(self, img, idx):
+        """ Forward computation
+
+        Args:
+        img (Tensor): Input images of shape (N, C, H, W).
+                Typically these should be mean centered and std scaled.
+            idx (Tensor): Index corresponding to each image.
+        
+        Returns:
+        logits (Tensor): logits of the data
+
+        """
+        features = self.encoder(img) 
+        features = nn.functional.normalize(features) # BxD
+        bs, feat_dim = features.shape[:2]
+        neg_idx = self.memory_bank.multinomial.draw(bs * self.hparams.num_negatives)
+
+        # Obtain positive features 
+        pos_feat = torch.index_select(self.memory_bank.feature_bank, 0,
+                                      idx)  # BXC
+
+        # Obtain negative features
+        neg_feat = torch.index_select(self.memory_bank.feature_bank, 0,
+                                      neg_idx).view(bs, self.neg_num,
+                                                    feat_dim)  # BxKxC
+        
+        # Obtain positive and negative logits of the data
+        pos_logits = torch.einsum('nc,nc->n',
+                                  [pos_feat, feature]).unsqueeze(-1)
+        neg_logits = torch.bmm(neg_feat, feature.unsqueeze(2)).squeeze(2)
+
+
+        logits = torch.cat([pos_logits, neg_logits], dim=1)
+        # apply temperature
+        logits /= self.hparams.softmax_temperature
+        # labels: positive key indicators
+        labels = torch.zeros(logits.shape[0], dtype=torch.long) # Nawid - class zero is always the correct class, which corresponds to the postiive examples in the logit tensor (due to the positives being concatenated first)
+        labels = labels.type_as(logits)
+
+        # update memory bank
+        with torch.no_grad():
+            self.memory_bank.update(idx, feature.detach())
+
+        return logits, labels
+
+
+
+    def loss_function(self, batch):
+        (img_1, img_2), labels,indices = batch
+        output, target = self(img=img_1,idx=indices)      
+        loss = F.cross_entropy(output, target.long())
+        acc1, acc5 = precision_at_k(output, target, top_k=(1, 5))
+        metrics = {'Loss': loss, 'Instance Accuracy @ 1': acc1, 'Instance Accuracy @ 5': acc5}
+
+        return metrics
 
     def training_step(self, batch, batch_idx):
         metrics = self.loss_function(batch)
