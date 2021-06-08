86c68f398dff9a163c896e82aa241ea98f1f23c8
nerdk312 <nawid.keshtmand@hotmail.com>
2021-06-08 08:34:18 +0100

    basic version of overlapping dataset mahalanobis distance able to be seen for the toy example

--- Contrastive_uncertainty/toy_replica/toy_general/run/general_run_setup.py
+++ Contrastive_uncertainty/toy_replica/toy_general/run/general_run_setup.py
@@ -1,7 +1,7 @@
 from re import search
 
 from Contrastive_uncertainty.toy_replica.toy_general.callbacks.general_callbacks import ModelSaving, MMD_distance
-from Contrastive_uncertainty.toy_replica.toy_general.callbacks.ood_callbacks import Mahalanobis_OOD, Aggregated_Mahalanobis_OOD, Differing_Mahalanobis_OOD 
+from Contrastive_uncertainty.toy_replica.toy_general.callbacks.ood_callbacks import Mahalanobis_OOD, Aggregated_Mahalanobis_OOD, Differing_Mahalanobis_OOD, Mahalanobis_OOD_Datasets 
 from Contrastive_uncertainty.toy_replica.toy_general.callbacks.visualisation_callback import Visualisation
 from Contrastive_uncertainty.toy_replica.toy_general.callbacks.metrics.metric_callback import MetricLogger, evaluation_metrics, evaltypes
 from Contrastive_uncertainty.toy_replica.toy_general.datamodules.datamodule_dict import dataset_dict
@@ -45,12 +45,18 @@ def callback_dictionary(Datamodule,config):
                     'Visualisation_instance_fine': Visualisation(Datamodule, vector_level='instance',label_level='fine',quick_callback=quick_callback)}
     
     # Automatically adding callbacks for the Mahalanobis distance for each different vector level as well as each different OOD dataset
+    # Collated list of OOD datamodules
+    Collated_OOD_datamodules = []
     for ood_dataset in config['OOD_dataset']:
         OOD_Datamodule = Datamodule_selection(dataset_dict, ood_dataset, config)
         OOD_callback = {f'Mahalanobis_instance_fine_{ood_dataset}':Mahalanobis_OOD(Datamodule,OOD_Datamodule,quick_callback=quick_callback,vector_level='instance', label_level='fine'),
                 f'Aggregated {ood_dataset}': Aggregated_Mahalanobis_OOD(Datamodule,OOD_Datamodule,quick_callback=quick_callback),
                 f'Differing {ood_dataset}': Differing_Mahalanobis_OOD(Datamodule,OOD_Datamodule,quick_callback=quick_callback)}
         callback_dict.update(OOD_callback)
+        Collated_OOD_datamodules.append(OOD_Datamodule)
+    
+    callback_dict.update({'OOD_Dataset_distances': Mahalanobis_OOD_Datasets(Datamodule, Collated_OOD_datamodules, quick_callback=quick_callback)})
+
     
     return callback_dict
 
