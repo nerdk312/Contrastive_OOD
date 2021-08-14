381ce824716edc5ecb9fcdc018f2601e9d251d06
nerdk312 <nawid.keshtmand@hotmail.com>
2021-08-14 19:24:22 +0100

    made duplicate code for the case of running the batch training multiple times

---
+++ Contrastive_uncertainty/experiments/run/duplicate/run_batch_training_3.py
@@ -0,0 +1,5 @@
+# Import general params
+from Contrastive_uncertainty.experiments.config.duplicate.base_params_3 import base_hparams
+from Contrastive_uncertainty.experiments.train.batch_train_experiments import batch_train
+
+batch_train(base_hparams)
